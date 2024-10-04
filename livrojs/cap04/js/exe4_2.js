56721de9f4d3d1b8216bbafad4b6ca6aa7a97cdd
iara <iaraotako@gmail.com>
2024-10-03 21:17:37 -0300

    arquivo index

--- livrojs/cap04/js/exe4_2.js
+++ livrojs/cap04/js/exe4_2.js
@@ -4,16 +4,21 @@ function calcular(){
     const feminino = document.getElementById("inFeminino").checked;
     const altura = document.getElementById("inAltura").value;
 
-    let peso;
+    let peso = feminino ? Math.pow(altura,2) * 21 : Math.pow(altura,2) *22;
 
-    if (feminino){
+   /* if (feminino){
 
         peso = 21 * Math.pow(altura, 2)
         } else {
         peso = 22 * Math.pow(altura, 2)
-        }
+        }*/
 
         document.querySelector("h3").innerText= `Peso ideal ${peso.toFixed(2)}`
     return false; //vai impedir o refresh da pagina equivalente ao e.preventDefault()
 
+} 
+
+function limpar(){
+    document.querySelector("h3").innerText = `${nome}: Peso ideal ${peso.toFixed(2)}`;
 }
+
