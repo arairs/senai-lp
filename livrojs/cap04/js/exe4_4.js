2b85502a1ad5389bd481f22f475713d7e1ef49f0
iara <iaraotako@gmail.com>
2024-10-04 21:46:43 -0300

    cap4 exemplos

---
+++ livrojs/cap04/js/exe4_4.js
@@ -0,0 +1,9 @@
+const frm = document.querySelector("form");
+const resp = document.querySelector ("h3");
+
+// "ouvinte" de evento, acionado quando o botão submit for clicado
+frm.addEventListener("submit", (e)=>{
+
+
+
+})
